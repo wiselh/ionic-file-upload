45e594c1fd72bb0701a91396fc9b301fa704a72b
wiselh <wise.self.2@gmail.com>
2018-05-06 16:58:36 +0100

    Upload Works fine
    $ ionic cordova plugin add cordova-plugin-camera
    $ npm install --save @ionic-native/camera
    $ npm install firebase --save
    Run App in Ionic DevApp in your phone   :
    $ ionic serve -c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import firebase from 'firebase';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDrEgCdYbjfwBLNaSEj7nkWyTQkbG46ZMA",
+  authDomain: "theme-app-b9976.firebaseapp.com",
+  databaseURL: "https://theme-app-b9976.firebaseio.com",
+  projectId: "theme-app-b9976",
+  storageBucket: "theme-app-b9976.appspot.com",
+  messagingSenderId: "954373264927"
+};
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
+
+  
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+
+      firebase.initializeApp(firebaseConfig);
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, Input } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  captureDataUrl: string;
+  alertCtrl: AlertController;
 
-  constructor(public navCtrl: NavController) {
+  @Input('useURI') useURI: Boolean = true;
 
+  constructor(public navCtrl: NavController, private camera: Camera, alertCtrl: AlertController) {
+    this.alertCtrl = alertCtrl;
+  }
+  getPicture(sourceType) {
+    const cameraOptions: CameraOptions = {
+      quality: 50,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: sourceType
+    };
+
+    this.camera.getPicture(cameraOptions)
+      .then((captureDataUrl) => {
+        this.captureDataUrl = 'data:image/jpeg;base64,' + captureDataUrl;
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  upload() {
+    let storageRef = firebase.storage().ref();
+    // Create a timestamp as filename
+    const filename = Math.floor(Date.now() / 1000);
+
+    // Create a reference to 'images/todays-date.jpg'
+    const imageRef = storageRef.child(`images/${filename}.jpg`);
+
+    imageRef.putString(this.captureDataUrl, firebase.storage.StringFormat.DATA_URL)
+      .then((snapshot) => {
+        // Do something here when the data is succesfully uploaded!
+        this.showSuccesfulUploadAlert();
+      });
+  }
+
+  showSuccesfulUploadAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Uploaded!',
+      subTitle: 'Picture is uploaded to Firebase',
+      buttons: ['OK']
+    });
+    alert.present();
+    // clear the previous photo data in the variable
+    this.captureDataUrl = "";
   }
 
 }
